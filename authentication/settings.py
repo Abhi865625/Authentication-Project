fcbd00bcdbd730a0fc11fbc74ac15d6da03f3873
Abhishek Kumar Mehta <akmehta7252@gmail.com>
2024-05-23 10:10:09 +0530

    modified:   authentication/settings.py

--- authentication/settings.py
+++ authentication/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7w(3_lomn6(1eq93$qjkv+-ykot2gcb_adt*i&_=_w7)-68&3k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
